f9766a39df612e2471c35249b28d7e838f25b9a9
LMGP2002 <luiguerrero@uniboyaca.edu.co>
2024-09-14 11:26:54 -0500

    Actualización filter service

--- src/app/services/filter.service.ts
+++ src/app/services/filter.service.ts
@@ -1,4 +1,4 @@
-import { HttpClient } from '@angular/common/http';
+import { HttpClient, HttpHeaders } from '@angular/common/http';
 import { inject, Injectable } from '@angular/core';
 import { firstValueFrom } from 'rxjs';
 import Swal from 'sweetalert2';
@@ -9,16 +9,22 @@ import Swal from 'sweetalert2';
 export class FilterService {
 
   private baseUrl = 'http://localhost:8080/api/v1/leads';
+  private http = inject(HttpClient);
 
-  private http=inject(HttpClient);
-
+  // Método para obtener el token JWT desde el almacenamiento local
+  private getToken(): string | null {
+    return localStorage.getItem('token'); // O donde sea que estés almacenando el token
+  }
 
   // Método para verificar si existen leads en el rango de fechas
   async checkLeadsExist(startDate: string, endDate: string): Promise<boolean> {
     const body = { startDate, endDate };
+    const headers = new HttpHeaders({
+      'Authorization': `Bearer ${this.getToken()}`  // Incluir el token JWT
+    });
 
     try {
-      const leads = await firstValueFrom(this.http.post<any[]>(`${this.baseUrl}/filter`, body));
+      const leads = await firstValueFrom(this.http.post<any[]>(`${this.baseUrl}/filter`, body, { headers }));
       return Array.isArray(leads) && leads.length > 0;
     } catch {
       Swal.fire({
@@ -34,15 +40,15 @@ export class FilterService {
   // Método para descargar el CSV
   downloadLeadsCSV(startDate: string, endDate: string): void {
     const body = { startDate, endDate };
+    const headers = new HttpHeaders({
+      'Authorization': `Bearer ${this.getToken()}`  // Incluir el token JWT
+    });
 
     this.http.post(`${this.baseUrl}/export`, body, {
+      headers,
       responseType: 'blob', // Recibir el archivo como blob
-      headers: {
-        'Content-Type': 'application/json',
-      },
     }).subscribe({
       next: (blob: Blob) => {
-        // Crea un enlace para descargar el archivo
         const a = document.createElement('a');
         const objectUrl = URL.createObjectURL(blob);
         a.href = objectUrl;
